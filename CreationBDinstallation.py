a1528ba2ba143bfcf01618b1efe3188200accc7c
CaillaudPA <E134892X@U-INF-J-E114-01.iut-nantes.univ-nantes.prive>
2015-03-05 11:17:13 +0100

    johk

--- CreationBDinstallation.py
+++ CreationBDinstallation.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python3
 
 import sqlite3
+from installation import Installation
 
 class CreationBDinstallation:
 
@@ -18,8 +19,34 @@ class CreationBDinstallation:
 					 otherTransport text, number_Equipements text, number_FicheEquipemenDateMaj text)''')
 
 		self.conn.commit()
-		self.conn.close()
+		#self.conn.close()
 
+	def ajoutInstallation(self, ins):
+		print('''INSERT INTO installations VALUES
+					   (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\", \"{5}\", {6}, {7}, {8}, \"{9}\", \"{10}\", \"{11}\", \"{12}\", \"{13}\", \"{14}\", \"{15}\", \"{16}\", 
+					   \"{17}\", \"{18}\", \"{19}\", \"{20}\", \"{21}\", \"{22}\", \"{23}\", \"{24}\", \"{25}\")'''.format(ins.town, ins.insee, ins.zipCode, ins.locality, ins.numbPath, ins.libellePath, ins.namePath,
+					   ins.longitude, ins.latitude, ins.accessibilityNothing, ins.accessibiliteHandiMoteur, ins.accessibiliteHandiSens,
+					   ins.empriseFonciere, ins.gardiennee, ins.multiCommune, ins.nbPlaceParking, ins.nbPlaceParkingHandi,
+					   ins.particularInstalation, ins.transportMetro, ins.transportBus, ins.transportTram, ins.transportTrain, ins.transportBoat,
+					   ins.otherTransport, ins.number_Equipements, ins.number_FicheEquipemenDateMaj))
+		self.c.execute('''INSERT INTO installations VALUES
+					   (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\", \"{5}\", {6}, {7}, {8}, \"{9}\", \"{10}\", \"{11}\", \"{12}\", \"{13}\", \"{14}\", \"{15}\", \"{16}\", 
+					   \"{17}\", \"{18}\", \"{19}\", \"{20}\", \"{21}\", \"{22}\", \"{23}\", \"{24}\", \"{25}\")'''.format(ins.town, ins.insee, ins.zipCode, ins.locality, ins.numbPath, ins.libellePath, ins.namePath,
+					   ins.longitude, ins.latitude, ins.accessibilityNothing, ins.accessibiliteHandiMoteur, ins.accessibiliteHandiSens,
+					   ins.empriseFonciere, ins.gardiennee, ins.multiCommune, ins.nbPlaceParking, ins.nbPlaceParkingHandi,
+					   ins.particularInstalation, ins.transportMetro, ins.transportBus, ins.transportTram, ins.transportTrain, ins.transportBoat,
+					   ins.otherTransport, ins.number_Equipements, ins.number_FicheEquipemenDateMaj))
+		self.conn.commit()
 
 tmp = CreationBDinstallation()
 tmp.creationTableInstallation()
+
+newInsta = Installation("hjio1", "hjio2", "hjio3", "hjio4", "hjio5", "h6jio", "7", 1.0, 1.2, "hj8io", "hji9o", 
+						"hji10o", "hj11io", "hj12io", "hji13o", "h14jio", "hj15io", "hji16o", "hji17o", "hj18io", "h19jio", 
+						"hji20o", "hji21o", "hjio22", "hjio23", "hjio24")
+
+
+
+tmp.ajoutInstallation(newInsta)
+
+
